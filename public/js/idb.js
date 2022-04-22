64fd5fcae1e58109764f80951f93455b2e042df2
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-04-21 22:15:23 -0400

    fix api route in idb.js file

--- public/js/idb.js
+++ public/js/idb.js
@@ -4,7 +4,7 @@ let db;
 const request = indexedDB.open('budget_tracker', 1);
 
 // create the object sotre that contains the data
-request.onupgradeneeded = (e) => {
+request.onupgradeneeded = function(e) {
   // save a ref to the db
   const db = e.target.result;
   // create an object store, set it to have auto-incrementing primary key
@@ -13,7 +13,7 @@ request.onupgradeneeded = (e) => {
 
 
 // upon a successful request
-request.onsuccess = (e) => {
+request.onsuccess = function(e) {
   // when db is created w its object store
   db = e.target.result;
   // check if app is online. if it is, send all local db data to api
@@ -23,35 +23,35 @@ request.onsuccess = (e) => {
 }
 
 // upon an error on request
-request.onerror = (e) => {
+request.onerror = function(e) {
   // just console.log the error
   console.log(e.target.errorCode);
 }
 
 // if user attempts to submit new entry and there's no internet connection
-let saveRecord = (record) => {
+function saveRecord(record) {
   // open a new transaction w db w read and write permission
   const transaction = db.transaction(['new_trans'], 'readwrite');
   // access the object store
-  const transactionObjectStore = transaction.objectStore('new_trans');
+  const transObjectStore = transaction.objectStore('new_trans');
 
   // add this new record to the object store
-  transactionObjectStore.add(record);
+  transObjectStore.add(record);
 }
 
 // a function that uploads the new transaction when we come back online
-let uploadTransaction = () => {
+function uploadTransaction() {
   // open a transaction on the db
   const transaction = db.transaction(['new_transaction'], 'readwrite');
   // access the object store
-  const transactionObjectStore = transaction.objectStore('new_transaction');
+  const transObjectStore = transaction.objectStore('new_transaction');
   // get all records from store
-  const getAll = transactionObjectStore.getAll();
+  const getAll = transObjectStore.getAll();
   // upon a successfull .getAll(), run this
-  getAll.onsuccess = () => {
+  getAll.onsuccess = function() {
     // if there's data in the store, send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/transaction', {
+      fetch('/api/transaction/bulk', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
@@ -67,9 +67,9 @@ let uploadTransaction = () => {
         // open another transaction
         const transaction = db.transaction(['new_trans'], 'readwrite');
         // access the new object store
-        const transactionObjectStore = transaction.objectStore('new_trans');
+        const transObjectStore = transaction.objectStore('new_trans');
         // clearr items in the store
-        transactionObjectStore.clear();
+        transObjectStore.clear();
         alert('All transactions have been submitted!');
       })
       .catch(err => {
